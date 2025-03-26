29190354f32600bbae6a8e64aeffee9e7d346a83
Synarcs <vedang.parasnis921@gmail.com>
2025-03-26 03:00:21 -0700

    cleaned btf vmlinux

---
+++ kernel/crypto/crypto_maps.h
@@ -0,0 +1,6 @@
+#ifndef __CRYPTO_MAPS_
+#define __CRYPTO_MAPS_ 
+
+
+
+#endif
--- kernel/crypto/lsm_bpf.c
+++ kernel/crypto/lsm_bpf.c
@@ -1,10 +1,16 @@
 #include <linux/bpf.h>
 #include <linux/version.h>
+#include <linux/lsm.h> 
 
 // libbpf 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_endian.h> 
+
+#include "../hdrs/consts.h"
+#include "../hdrs/utils.h"
+#include "crypto_maps.h"
 
 /*
     Uses PKCS#7 keyring to verify the pogram source before injecting 
